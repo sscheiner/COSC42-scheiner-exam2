f7aee39fdf78303624094c60fb490282c8197916
sscheiner <sf.scheiner@gmail.com>
2018-05-19 11:49:50 -0400

    add Inventory and BatAI

---
+++ ChangedFilesForExam/BatAi.java
@@ -0,0 +1,14 @@
+package game;
+
+public class BatAi extends CreatureAi {
+
+	public BatAi(Creature creature) {
+		super(creature);
+	}
+
+	@Override
+	public void onUpdate(){
+		wander();
+		wander();
+	}
+}
